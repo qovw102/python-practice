796002518111407581a7c54d80cd1abeb52e629e
Nelson Ye <qovw102@gmail.com>
2019-05-02 14:40:14 +0800

    add guess times function

--- guess_number.py
+++ guess_number.py
@@ -1,15 +1,17 @@
 #random number practice, game of guess number
 import random
 r = random.randint(1,100)
-
+count = 0
 while True:
+    count += 1
     num = input('Please enter number:')
     num = int(num)
     if num == r:
         print('Good Job!')
         break
     elif num > r:
-        print('num is bigger than answer')
+        print('number is bigger than answer')
     elif num < r:
-        print('num is smaller than answer')
+        print('number is smaller than answer')
+    print(count, 'times guess')
 
